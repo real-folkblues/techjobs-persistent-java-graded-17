0ac366f77e754df0488772ca8c69e1890fd83695
real-folkblues <marenicolor@gmail.com>
2023-11-06 18:59:54 -0500

    connected to mysql database, added job and job_seq table columns to quieries.sql. Task 1 a wrap.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
---Part 1
+--Part 1 Table: job
+         Columns:
+         id int PK
+         employer varchar(255)
+         name varchar(255)
+         skills varchar(255)
+         job_seq table: next_val bigint
 
 --Part 2
 
