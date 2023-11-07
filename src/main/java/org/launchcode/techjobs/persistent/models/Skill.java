f83196e92182fe7f876a9cb3b503123eadbd097d
real-folkblues <marenicolor@gmail.com>
2023-11-06 20:10:58 -0500

    Task 2, Created Skill/Employer/Crud Interfaces, added annos.

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotNull
+    @Size(min = 1)
+    private String description;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Skill(String description){
+
+    }
+
 
 }
---
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/SkillRepository.java
@@ -0,0 +1,7 @@
+package org.launchcode.techjobs.persistent.models.data;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkillRepository extends @CrudRepository {
+}
